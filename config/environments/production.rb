f45d1852b3c9424e3f651ff43390e01852530ec7
naotosun <naotosun0820@gmail.com>
2023-10-26 19:18:17 +0900

    comp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+server '52.196.197.21', user: 'ec2-user', roles: %w{app db web}
